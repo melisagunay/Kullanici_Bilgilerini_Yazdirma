d2f48acc30c6bc746e34b354883a41bba1ff766f
melisagunay <51082351+melisagunay@users.noreply.github.com>
2022-02-07 00:50:12 +0300

    Add files via upload

---
+++ 1 - kullanici biilgilerini yazdirma/1 - kullanici biilgilerini yazdirma/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace _1___kullanici_biilgilerini_yazdirma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtGelir = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.rdbCalisiyor = new System.Windows.Forms.RadioButton();
+            this.rdbCalismiyor = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(160, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(128, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyad =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(126, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gelir =";
+            // 
+            // txtGelir
+            // 
+            this.txtGelir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtGelir.Location = new System.Drawing.Point(220, 217);
+            this.txtGelir.Name = "txtGelir";
+            this.txtGelir.Size = new System.Drawing.Size(245, 30);
+            this.txtGelir.TabIndex = 4;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSoyad.Location = new System.Drawing.Point(220, 130);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(245, 30);
+            this.txtSoyad.TabIndex = 5;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAd.Location = new System.Drawing.Point(220, 90);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(245, 30);
+            this.txtAd.TabIndex = 6;
+            // 
+            // rdbCalisiyor
+            // 
+            this.rdbCalisiyor.AutoSize = true;
+            this.rdbCalisiyor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rdbCalisiyor.Location = new System.Drawing.Point(220, 179);
+            this.rdbCalisiyor.Name = "rdbCalisiyor";
+            this.rdbCalisiyor.Size = new System.Drawing.Size(103, 29);
+            this.rdbCalisiyor.TabIndex = 7;
+            this.rdbCalisiyor.TabStop = true;
+            this.rdbCalisiyor.Text = "calisiyor";
+            this.rdbCalisiyor.UseVisualStyleBackColor = true;
+            // 
+            // rdbCalismiyor
+            // 
+            this.rdbCalismiyor.AutoSize = true;
+            this.rdbCalismiyor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rdbCalismiyor.Location = new System.Drawing.Point(346, 179);
+            this.rdbCalismiyor.Name = "rdbCalismiyor";
+            this.rdbCalismiyor.Size = new System.Drawing.Size(119, 29);
+            this.rdbCalismiyor.TabIndex = 8;
+            this.rdbCalismiyor.TabStop = true;
+            this.rdbCalismiyor.Text = "calismiyor";
+            this.rdbCalismiyor.UseVisualStyleBackColor = true;
+            this.rdbCalismiyor.CheckedChanged += new System.EventHandler(this.rdbCalismiyor_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(346, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 47);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "yazdir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 389);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rdbCalismiyor);
+            this.Controls.Add(this.rdbCalisiyor);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtGelir);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtGelir;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.RadioButton rdbCalisiyor;
+        private System.Windows.Forms.RadioButton rdbCalismiyor;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 1 - kullanici biilgilerini yazdirma/1 - kullanici biilgilerini yazdirma/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1___kullanici_biilgilerini_yazdirma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ad = txtAd.Text;
+            string soyad = txtSoyad.Text;
+            float gelir ;
+            gelir = Convert.ToSingle((rdbCalisiyor.Checked) ? txtGelir.Text: "0");
+            string calismaDurumu =  rdbCalisiyor.Checked ? "Calisiyor" : "Calismiyor";           
+                MessageBox.Show("ad ; " + ad +
+                            "\nsoyad ; " + soyad + 
+                            "\nbakiye ; " + gelir + 
+                            "\ncalisma durumu ; " + calismaDurumu);
+
+        }
+
+        private void rdbCalismiyor_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbCalismiyor.Checked)
+            {
+                txtGelir.Text = "";
+                txtGelir.Enabled = false;
+            }
+            else
+            {
+                txtGelir.Enabled = true;
+            }
+        }
+
+       
+    }
+}
